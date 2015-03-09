47e9ef24cd56c26a24a7261e37e7e4116c425a9b
Yoeri Van Damme <yoeri.vandamme@gmail.com>
2015-03-09 22:08:54 +0100

    added navigationpage

--- ManlyManFoodClient/ManlyManFoodClient/App.cs
+++ ManlyManFoodClient/ManlyManFoodClient/App.cs
@@ -12,7 +12,7 @@ namespace ManlyManFoodClient
 		public App()
 		{
 			// The root page of your application
-			MainPage = new MainPage();
+			MainPage = new NavigationPage(new MainPage());
 		}
 
 		protected override void OnStart()
